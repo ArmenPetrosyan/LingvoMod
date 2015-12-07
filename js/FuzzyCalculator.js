9229955b606dd64cd7fa0e0efe7ac4abfac63e36
ArmenPetrosyan <thebraincrasher@gmail.com>
2015-12-07 19:06:15 +0200

    UPD: сделал 2 варианта

---
+++ js/FuzzyCalculator.js
@@ -0,0 +1,160 @@
+// класс обработчика нечетких множеств
+function FuzzyCalculator(memberFuncStr, fuzzySet){
+
+    var triangleNum = fuzzySet || {min:0, x:5, max:10},
+        stepCount   = 100,
+        a           = 0,
+        c           = 0;
+
+    var memberFunc  = function(){}; // функция истинности ( объявление )
+
+    switch (memberFuncStr) {        // функция истинности ( инициализация )
+
+        case 'sigma' :            // опционально сигмоидная ;)
+            memberFunc = function(x){
+                return 1 / (1 + Math.exp(-  a * (x - c))) || 0;
+            };
+            break;
+
+        default :                   // по умолчанию треугольная
+            memberFunc = function(x){
+                var result = 0;
+
+                if (triangleNum.min <= x && x <= triangleNum.x){
+                    result = (x - triangleNum.min) / (triangleNum.x - triangleNum.min);
+                }
+                else
+                if(triangleNum.x <= x && x <= triangleNum.max){
+                    result = (triangleNum.max - x) / (triangleNum.max - triangleNum.x);
+                }
+
+                return result;
+            };
+    }
+
+    // Интерфейс управления классом
+    return {
+
+        setTriangleNum : function(min, x, max){
+            triangleNum.min = min;
+            triangleNum.x   = x;
+            triangleNum.max = max;
+        },
+
+        setSigmoidParams : function(aa,cc){
+            a = aa;
+            c = cc;
+        },
+
+        getSet      : function(){
+            var path = [];
+
+            for (var x = 0; x < stepCount; x++){
+                var pow = memberFunc(x);
+                path.push([x, pow]);
+            }
+
+            return path;
+        },
+
+        // Висота нечіткої множини А
+        fuzzyHeight : function(){
+            var max = 0;
+
+            for (var x = 0; x < stepCount; x++){
+                if( max < memberFunc(x) ){
+                    max = x;
+                }
+            }
+
+            return max;
+        },
+
+        // Носій нечіткої множини А
+        support      : function(){
+            var path = [];
+
+            for (var x = 0; x < stepCount; x++){
+                var pow = memberFunc(x);
+                if( pow > 0 ){
+                    path.push([x, pow]);
+                }
+            }
+
+            return path;
+        },
+
+        // Ядро нечіткої множини А
+        core      : function(){
+            var path = [];
+
+            for (var x = 0; x < stepCount; x++){
+                var pow = memberFunc(x);
+                if( pow == 1 ){
+                    path.push([x, pow]);
+                }
+            }
+
+            return path;
+        },
+
+        // Потужність (кардинальне число) нечіткої множини А
+        card       : function(){
+            var sum = 0;
+
+            for (var x = 0; x < stepCount; x++){
+                var pow = memberFunc(x);
+                sum += pow;
+            }
+
+            return sum;
+        },
+
+        // Оператор концентрації (concentration)
+        modCon        : function(){
+            var path = [];
+
+            for (var x = 0; x < stepCount; x++){
+                var pow = Math.pow(memberFunc(x),2);
+
+                path.push([x, pow]);
+
+            }
+
+            return path;
+        },
+
+        // Оператор розтягнення (dilatation)
+        modDil         : function(){
+            var path = [];
+
+            for (var x = 0; x < stepCount; x++){
+                var pow = Math.sqrt(memberFunc(x));
+
+                path.push([x, pow]);
+
+            }
+
+            return path;
+        },
+
+        // Оператор збільшення контрастності
+        modInt       : function(){
+            var path = [];
+
+            for (var x = 0; x < stepCount; x++){
+                var pow = memberFunc(x);
+
+                if (pow < 0.5){
+                    pow = 2 * Math.pow(pow,2);
+                }else {
+                    pow = 1 - (2 * Math.pow((1 - pow),2));
+                }
+
+                path.push([x, pow]);
+            }
+
+            return path;
+        },
+    }
+}
---
+++ js/common.js
@@ -0,0 +1,73 @@
+;(function($){
+
+    var functionStr = $('.tab-control.active').data('function');
+    var instance = new FuzzyCalculator(functionStr);
+    //refresh();
+
+    $('.tab-control').on('click', function () {
+        functionStr = $(this).data('function');
+        instance = new FuzzyCalculator(functionStr);
+        //refresh();
+    });
+
+    $('.js-actionButton')
+        .on('click', function(){
+            var tab = $('.tab-control.active').data('function');
+
+            if(tab == 'triangle'){
+                var triangleMin = + $('#triangleMin').val() || 0;
+                var triangleX = + $('#triangleX').val() || 0;
+                var triangleMax = + $('#triangleMax').val() || 0;
+
+                // Обновляем 3угольное число
+                if( (triangleMin < triangleX) && (triangleX < triangleMax) ){
+                    instance.setTriangleNum(triangleMin, triangleX, triangleMax);
+                }else{
+                    $('#error-modal').modal();
+                    return false;
+                }
+            }else
+            if(tab == 'sigma'){
+                var a = + $('#sigA').val() || 0;
+                var c = + $('#sigC').val() || 0;
+                instance.setSigmoidParams(a,c);
+            }
+
+            var modificatorStr =  $('.active .operators').val();
+            var resultMatrix = instance.getSet();
+
+            // если применен модификатор
+            if(modificatorStr != 0){
+                switch (modificatorStr) {
+                    case 'con':
+                        resultMatrix = instance.modCon();
+                        break;
+                    case 'dil':
+                        resultMatrix = instance.modDil();
+                        break;
+                    case 'int':
+                        resultMatrix = instance.modInt();
+                        break;
+                }
+
+                $.plot($(".active .triangle-chart"), [{label:"A", data:resultMatrix}], {  xaxis: { min: 0 },yaxis: { max: 1.5 } });
+            }else{
+                var card = instance.card().toFixed(2);
+                $('.active .card').text(card);
+
+
+                var support = instance.support(),
+                    core   = instance.core();
+
+                $.plot($(".active .triangle-chart"), [
+                    {label:"A", data:resultMatrix},
+                    {label:"Носій А", data:support},
+                    {label:"Ядро нечіткої множини А", data:core},
+                ], {  xaxis: { min: 0 },yaxis: { max: 1.5 } });
+
+            }
+
+
+        });
+
+})(jQuery);
